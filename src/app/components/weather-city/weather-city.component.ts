fe0e5df9279fd8831f969c621261657c864f563b
Hậu Trần <hautrantrung.02@gmail.com>
2025-02-15 13:47:46 +0700

    main layout

--- src/app/components/weather-city/weather-city.component.ts
+++ src/app/components/weather-city/weather-city.component.ts
@@ -1,18 +1,21 @@
-import { HttpClient, HttpHandler } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, signal } from '@angular/core';
-import { FormsModule, NgModel } from '@angular/forms';
-import { TuiTextfield } from '@taiga-ui/core';
+import { FormsModule } from '@angular/forms';
+import { TuiAppearance, TuiTextfield } from '@taiga-ui/core';
 import { environment } from '../../../environments/environment';
+import { WeatherData } from '../../../models/weather';
+import { TuiCardMedium } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-weather-city',
-  imports: [TuiTextfield, FormsModule],
+  imports: [TuiTextfield, FormsModule, TuiCardMedium, TuiAppearance],
   templateUrl: './weather-city.component.html',
   styleUrl: './weather-city.component.scss',
   providers: [],
 })
 export class WeatherCityComponent implements OnInit {
   city = signal('');
+  data = signal<WeatherData | null>(null);
 
   constructor(private http: HttpClient) {
     this.http
@@ -20,7 +23,7 @@ export class WeatherCityComponent implements OnInit {
         params: { key: environment.apiKey, q: 'London', api: 'no' },
       })
       .subscribe((res) => {
-        console.log(res);
+        this.data.set(res);
       });
   }
 
@@ -28,7 +31,5 @@ export class WeatherCityComponent implements OnInit {
 
   onChangeCity(newValue: string) {
     this.city.set(newValue);
-
-    console.log(this.city());
   }
 }
